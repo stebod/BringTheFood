86a141f42d09561d9cf4e958579e90a06754b1ef
federicobadini <Sup3r4ed91>
2015-06-23 13:43:51 +0200

    awful graphical fixing

---
+++ Bring the Food/notificationSettingsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  notificationSettingsViewController.swift
+//  Bring the Food
+//
+//  Created by federico badini on 23/06/15.
+//  Copyright (c) 2015 Federico Badini, Stefano Bodini. All rights reserved.
+//
+
+import UIKit
+
+class notificationSettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    @IBAction func cancelButtonPressed(sender: UIButton) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+}
