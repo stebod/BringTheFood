f6ca0e34a5d668684776903e9948927e376cd8fc
Strefano Bodini <bodini.stefano@gmail.com>
2015-06-27 21:56:52 +0200

    commenti image downloader

--- Bring the Food/ImageDownloader.swift
+++ Bring the Food/ImageDownloader.swift
@@ -10,25 +10,31 @@ import Foundation
 import UIKit
 
 
-/*
-    Esegue il download dalla url con cui è stato inizializzato.
-    La politica di caching assicura che ogni immagine sia scaricata al più una volta dal server.
-    Fatto questo, invia una notifica, il cui stato segnala se il download è andato a buon fine.
-*/
-
+/// An object of this type has to be instantiated specifically for the 
+/// download of a single image, whose url is specified in the initializer.
 public class ImageDownloader{
     
     private let url:String!
     private var image: UIImage?
     
+    /// :param: url the url of the image that has to be downloaded
     public init(url:String!){
         self.url = url
     }
     
+    /// Downloads the image whose url has been specified in the initializer.
+    /// When the download is completed, a notification will be posted in
+    /// NSNotificationCenter, with the current instance ImageDownloader as
+    /// object of the notification. After receiving the notification, it will
+    /// be possible to retreive the image using the method getImage()
     public func downloadImage(){
         RestInterface.getInstance().downloadImage(self.url, imDownloader: self)
     }
     
+    /// This is the method called by the RestInterface when a response to the 
+    /// image download request is received. This method uses the data received
+    /// to set the image, and sends a notification to NSNotificationCenter, indicating
+    /// if the download succeded.
     public func setImage(data: NSData!, response: NSURLResponse!, error:NSError!){
         
         if(error == nil && data != nil){
@@ -50,6 +56,8 @@ public class ImageDownloader{
 
     }
     
+    /// Returns the image that has been downloaded, or nil
+    /// if the download hasn't been completed yet
     public func getImage() -> UIImage?{
         return self.image
     }
