34b51fa0560a05edf8ecb3d65747a19ff3f19257
federicobadini <Sup3r4ed91>
2015-07-01 16:27:06 +0200

    fix

---
+++ Bring the Food/NotificationsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationsViewController.swift
+//  Bring the Food
+//
+//  Created by federico badini on 01/07/15.
+//  Copyright (c) 2015 Federico Badini, Stefano Bodini. All rights reserved.
+//
+
+import UIKit
+
+class NotificationsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+}
